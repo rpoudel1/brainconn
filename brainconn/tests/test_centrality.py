8d8b01348adda29beaf4209b42d8c7464b035dd2
Taylor Salo <tsalo006@fiu.edu>
2018-07-29 18:55:11 -0400

    Update test.

--- brainconn/tests/test_centrality.py
+++ brainconn/tests/test_centrality.py
@@ -3,7 +3,7 @@
 import os.path as op
 
 import numpy as np
-import brainconn as bc
+from brainconn import centrality
 from brainconn.tests.utils import (load_sample, MAT_DIR)
 
 
@@ -14,7 +14,7 @@ def test_pc():
 
     pc = np.load(op.join(MAT_DIR, 'sample_pc.npy'))
 
-    pc_ = bc.centrality.participation_coef(x, ci)
+    pc_ = centrality.participation_coef(x, ci)
     print(list(zip(pc, pc_)))
 
     assert np.allclose(pc, pc_, atol=0.02)
@@ -24,29 +24,29 @@ def participation_test():
     W = np.eye(3)
     ci = np.array([1, 1, 2])
 
-    assert np.allclose(bc.centrality.participation_coef(W, ci), [0, 0, 0])
-    assert np.allclose(bc.centrality.participation_coef_sign(W, ci)[0],
+    assert np.allclose(centrality.participation_coef(W, ci), [0, 0, 0])
+    assert np.allclose(centrality.participation_coef_sign(W, ci)[0],
                        [0, 0, 0])
 
     W = np.ones((3, 3))
-    assert np.allclose(bc.centrality.participation_coef(W, ci), [
+    assert np.allclose(centrality.participation_coef(W, ci), [
         0.44444444, 0.44444444, 0.44444444])
-    assert np.allclose(bc.centrality.participation_coef_sign(W, ci)[0], [
+    assert np.allclose(centrality.participation_coef_sign(W, ci)[0], [
         0.44444444, 0.44444444, 0.44444444])
 
     W = np.eye(3)
     W[0, 1] = 1
     W[0, 2] = 1
-    assert np.allclose(bc.centrality.participation_coef(W, ci),
+    assert np.allclose(centrality.participation_coef(W, ci),
                        [0.44444444, 0, 0])
-    assert np.allclose(bc.centrality.participation_coef_sign(W, ci)[0],
+    assert np.allclose(centrality.participation_coef_sign(W, ci)[0],
                        [0.44444444, 0, 0])
 
     W = np.eye(3)
     W[0, 1] = -1
     W[0, 2] = -1
     W[1, 2] = 1
-    assert np.allclose(bc.centrality.participation_coef_sign(W, ci)[0],
+    assert np.allclose(centrality.participation_coef_sign(W, ci)[0],
                        [0.,  0.5,  0.])
 
 
@@ -54,12 +54,12 @@ def gateway_test():
     x = load_sample(thres=.1)
     ci = np.load(op.join(MAT_DIR, 'sample_partition.npy'))
 
-    g_pos, _ = bc.centrality.gateway_coef_sign(x, ci)
+    g_pos, _ = centrality.gateway_coef_sign(x, ci)
 
     print(np.sum(g_pos), 43.4382)
     assert np.allclose(np.sum(g_pos), 43.4382, atol=.001)
 
-    gpb, _ = bc.centrality.gateway_coef_sign(x, ci,
+    gpb, _ = centrality.gateway_coef_sign(x, ci,
                                              centrality_type='betweenness')
 
     print(np.sum(gpb), 43.4026)
@@ -72,7 +72,7 @@ def test_zi():
 
     zi = np.load(op.join(MAT_DIR, 'sample_zi.npy'))
 
-    zi_ = bc.centrality.module_degree_zscore(x, ci)
+    zi_ = centrality.module_degree_zscore(x, ci)
     print(list(zip(zi, zi_)))
 
     assert np.allclose(zi, zi_, atol=0.05)
@@ -88,7 +88,7 @@ def test_zi():
 def test_shannon_entropy():
     x = load_sample(thres=0.4)
     ci = np.load(op.join(MAT_DIR, 'sample_partition.npy'))
-    hpos, _ = bc.centrality.diversity_coef_sign(x, ci)
+    hpos, _ = centrality.diversity_coef_sign(x, ci)
     print(np.sum(hpos))
     print(hpos[-1])
     assert np.allclose(np.sum(hpos), 102.6402, atol=.01)
